a5ab19fe73048123b111a6033a151fd80073cfa9
Cheru Berhanu <cheru@cheru.dev>
2024-05-16 14:54:49 -0400

    fixes

--- components/Logo.js
+++ components/Logo.js
@@ -2,8 +2,9 @@ export default function Logo({ size }) {
   return (
     <div id="logo" className={size}>
       <img src="/logo.webp"/>
-      <div style={{marginLeft: "10px"}}>
-        <h1>Monolith BCI</h1>
+      <div style={{marginLeft: "5px"}}>
+        <h1>MONOLITH</h1>
+	<h1>BRAIN-COMPUTER INTERFACES</h1>
       </div>
     </div>
   )
